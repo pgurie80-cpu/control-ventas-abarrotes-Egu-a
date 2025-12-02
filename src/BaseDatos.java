f75c9fb2e5a85807a067e56dcfde089670cfb06e
pgurie80-cpu <pgurie80@gmail.com>
2025-12-01 20:44:07 -0600

    Create BaseDatos class with database methods

---
+++ src/BaseDatos.java
@@ -0,0 +1,26 @@
+public class BaseDatos {
+
+    // Constructor
+    public BaseDatos() {
+    }
+
+    // Método para conectar a la base de datos
+    public void conectar() {
+        // TODO: Implementar en la versión final
+    }
+
+    // Método para guardar datos (ej. ventas)
+    public void guardarDatos(String coleccion, String datos) {
+        // TODO: Implementar en la versión final
+    }
+
+    // Método para leer datos
+    public void leerDatos(String coleccion) {
+        // TODO: Implementar en la versión final
+    }
+
+    // Método para cerrar la conexión
+    public void cerrarConexion() {
+        // TODO: Implementar en la versión final
+    }
+}
