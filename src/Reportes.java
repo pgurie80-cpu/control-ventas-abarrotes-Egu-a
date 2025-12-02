1473aa8defa952352f46d5669111e86871d7ff61
pgurie80-cpu <pgurie80@gmail.com>
2025-12-01 20:41:49 -0600

    Create Reportes class with report generation methods

---
+++ src/Reportes.java
@@ -0,0 +1,21 @@
+public class Reportes {
+
+    // Constructor
+    public Reportes() {
+    }
+
+    // Método para generar reporte diario
+    public void generarReporteDiario() {
+        // TODO: Implementar en versión final
+    }
+
+    // Método para generar reporte semanal
+    public void generarReporteSemanal() {
+        // TODO: Implementar en versión final
+    }
+
+    // Método para generar reporte mensual
+    public void generarReporteMensual() {
+        // TODO: Implementar en versión final
+    }
+}
